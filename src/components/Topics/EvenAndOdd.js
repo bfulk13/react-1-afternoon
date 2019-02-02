6fdd6f18c9ecbd50cf2eee2c67c5aea019d57d7b
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-02-01 19:43:33 -0700

    thru-step4

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class EvenAndOdd extends Component{
+    constructor(){
+      super();
+      this.state = {
+        evenArray: [],
+        oddArray: [],
+        userInput: ``
+      }
+    }
+    handleChange(val){
+      this.setState({ userInput: val })
+    }
+
+    assignEvenAndOdds(input){
+      let userArr = input.split(`,`);
+      userArr = userArr.map(input => parseInt(input,10));
+      const evens = userArr.filter( (input) => input % 2 === 0);
+      const odds = userArr.filter( (input) => input % 2 !== 0);
+      this.setState({
+        evenArray: evens,
+        oddArray: odds,
+        userInput: ``
+      })
+    }
+
+    render(){
+      return(
+        <div className="puzzleBox evenAndOddPB">
+          <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) } value={this.state.userInput}/>
+          <button className="confirmationButton" onClick={ () => this.assignEvenAndOdds(this.state.userInput) }> Split </button>
+          <span className="resultsBox"> Evens: { JSON.stringify(this.state.evenArray) }</span>
+          <span className="resultsBox"> Odds: { JSON.stringify(this.state.oddArray) }</span>
+          <h4>Evens and Odds</h4>
+        </div>
+      )
+    }
+}
+
+export default EvenAndOdd;
---
+++ src/components/Topics/Palindrome.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Palindrome extends Component{
+    
+    render(){
+      return(
+        <div></div>
+      )
+    }
+}
+
+export default Palindrome;
